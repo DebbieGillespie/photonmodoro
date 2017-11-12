e685d235273282e2e0c2844125c55a4a59e972ed
Debbie Gillespie <debbie@particle.io>
2017-11-11 18:14:54 -0600

    Add ability to track state
    The LED is mostly working. When I initially push the button, the LED
    turns on and stays on. Hitting the button again turns it off. However,
    we get random weird cases where pushing the button has no effect.

--- main.ino
+++ main.ino
@@ -1,7 +1,8 @@
 int led = D7;
 int button = D0;
 int pullup = D2;
-int val = 0;
+int ledOn = LOW;
+int toggle = LOW;
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -11,11 +12,28 @@ void setup() {
 }
 
 void loop() {
-    val = digitalRead(button);
 
-    if(val == HIGH) {
-        digitalWrite(led, HIGH);
+    if(digitalRead(button)) {
+        toggle = HIGH;
     } else {
-      digitalWrite(led, LOW);
+        toggle = LOW;
     }
+
+    digitalWrite(led, ledOn);
+
+    if(toggle) {
+        ledOn = toggleState(ledOn);
+        toggle = LOW;
+    }
+
+    delay(1);
+}
+
+int toggleState(int currentState) {
+  if (currentState) {
+    return LOW;
+  }
+  else {
+    return HIGH;
+  }
 }
